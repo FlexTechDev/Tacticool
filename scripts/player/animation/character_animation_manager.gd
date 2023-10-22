325076f5ac62c48e60fb0f82907fff414991e3b6
x32dev <120539187+FlexTechDev@users.noreply.github.com>
2023-10-22 10:10:21 -0500

    animation graph refactor for weapons

--- scripts/player/animation/character_animation_manager.gd
+++ scripts/player/animation/character_animation_manager.gd
@@ -2,6 +2,10 @@ extends Node3D
 
 class_name AnimationManager
 
+const WeaponType = {
+	NoGun = "NoGun",
+}
+
 @export var tree: AnimationTree;
 @export var playback_speed: float = 1;
 @export var y_velocity_buffer: float = 0.1;
@@ -10,6 +14,17 @@ class_name AnimationManager
 @export var look_ik_max_down_rotation: float = -15;
 @export var look_ik_max_up_rotation: float = 35;
 @export var enable_head_ik: bool = false;
+@export var current_weapon = WeaponType.NoGun;
+
+func _ready() -> void:
+	set_weapon_type("NoGun");
+
+func set_weapon_type(type: String) -> void:
+	for t in WeaponType:
+		if(t == type):
+			tree.set("parameters/MasterState/conditions/" + str(t), true)
+		else:
+			tree.set("parameters/MasterState/conditions/" + str(t), false)
 
 func _process(delta: float) -> void:
 	if(enable_head_ik):
@@ -20,30 +35,30 @@ func _process(delta: float) -> void:
 func move(vector: Vector2, y_velocity: float) -> void:
 	if(abs(y_velocity) <= y_velocity_buffer):
 		playback_speed = 3;
-		tree.set("parameters/legs/grounded/blend_position", lerp(tree.get("parameters/legs/grounded/blend_position"), vector, 0.05));
+		tree.set("parameters/MasterState/" + str(current_weapon) + "/Walk/blend_position", lerp(tree.get("parameters/MasterState/" + str(current_weapon) + "/Walk/blend_position"), vector, 0.05));
 		set_falling(false);
 	elif(abs(y_velocity) > y_velocity_buffer):
 		playback_speed = 3;
 		set_falling(true);
 
 func set_sliding(value: bool) -> void:
-	if(tree.get("parameters/legs/conditions/not_falling")):
-		tree.set("parameters/legs/conditions/sliding", value);
-		tree.set("parameters/legs/conditions/not_sliding", !value);
+	if(tree.get("parameters/MasterState/" + str(current_weapon) + "/conditions/not_falling")):
+		tree.set("parameters/MasterState/" + str(current_weapon) + "/conditions/sliding", value);
+		tree.set("parameters/MasterState/" + str(current_weapon) + "/conditions/not_sliding", !value);
 
 func set_falling(value: bool) -> void:
-	tree.set("parameters/legs/conditions/falling", value);
-	tree.set("parameters/legs/conditions/not_falling", !value);
+	tree.set("parameters/MasterState/" + str(current_weapon) + "/conditions/falling", value);
+	tree.set("parameters/MasterState/" + str(current_weapon) + "/conditions/not_falling", !value);
 
 func vault(dead_time: float) -> void:
 	playback_speed = 3;
-	tree.set("parameters/legs/conditions/vaulting", true);
-	tree.set("parameters/legs/conditions/not_vaulting", false);
+	tree.set("parameters/MasterState/" + str(current_weapon) + "/conditions/vaulting", true);
+	tree.set("parameters/MasterState/" + str(current_weapon) + "/conditions/not_vaulting", false);
 	
 	await get_tree().create_timer(dead_time).timeout;
 	
-	tree.set("parameters/legs/conditions/vaulting", false);
-	tree.set("parameters/legs/conditions/not_vaulting", true);
+	tree.set("parameters/MasterState/" + str(current_weapon) + "/conditions/vaulting", false);
+	tree.set("parameters/MasterState/" + str(current_weapon) + "/conditions/not_vaulting", true);
 
 func process_head_ik() -> void:
 	var bone_id: int = skeleton.find_bone("Gut");
